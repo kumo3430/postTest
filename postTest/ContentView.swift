a91bc00e04aa28b39387680aaf288654d74f273f
Yun <3430yun>
2023-03-06 14:31:57 +0800

    Initial Commit

---
+++ postTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  postTest
+//
+//  Created by 呂沄 on 2023/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ postTest/postTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  postTestApp.swift
+//  postTest
+//
+//  Created by 呂沄 on 2023/3/6.
+//
+
+import SwiftUI
+
+@main
+struct postTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
