02b8e6378e27bfbba644fb74fe32af80b62801b8
Yun <3430yun>
2023-03-06 14:35:35 +0800

    原始黨

--- postTest/ContentView.swift
+++ postTest/ContentView.swift
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var email: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            TextField("Username", text: $username)
+            SecureField("Password", text: $password)
+            TextField("Email", text: $email)
+            Button(action: {
+                registerUser()
+            }, label: {
+                Text("Register")
+            })
         }
-        .padding()
+    }
+    
+    func registerUser() {
+        let userData = ["username": username, "password": password, "email": email]
+        guard let url = URL(string: "http://your-api-url.com/register") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: userData, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // handle response
+        }.resume()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
