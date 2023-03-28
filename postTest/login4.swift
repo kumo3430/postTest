7899ee594658a2ae247e95f331c7e9a628f04070
Yun <3430yun>
2023-03-28 20:45:11 +0800

    username顯示

--- postTest/login4.swift
+++ postTest/login4.swift
@@ -6,11 +6,18 @@ struct login4: View {
     @State private var password = ""
     @State private var isLoggedIn = false
     @State private var errorMessage = ""
+    @State private var getUsername = ""
+    
+    struct UserData : Decodable {
+        var id: String
+        var username: String
+    }
     
     var body: some View {
         VStack {
             if isLoggedIn {
                 Text("You are logged in!")
+                Text(getUsername)
                 Button("Log out") {
                     logout()
                 }
@@ -37,8 +44,8 @@ struct login4: View {
 //        }
         let url = URL(string: "http://127.0.0.1:8888/register/register3.php")!
         var request = URLRequest(url: url)
-        print(url)
-        print(request)
+//        print(url)
+//        print(request)
         request.httpMethod = "POST"
         let body = ["username": username, "password": password]
         let jsonData = try! JSONSerialization.data(withJSONObject: body, options: [])
@@ -75,44 +82,46 @@ struct login4: View {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         let body = ["username": username, "password": password]
+        
         let jsonData = try! JSONSerialization.data(withJSONObject: body, options: [])
         request.httpBody = jsonData
         URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data,
-              let content = String(data: data, encoding: .utf8) {
-               print(content)
-//                if content == "登錄失敗0 個結果" {
-//                    isLoggedIn = false
-//                    Text("帳號或密碼輸入錯誤")
-//                    print("帳號或密碼輸入錯誤")
-//                } else{
-//                    isLoggedIn = true
-//                }
+            
+            guard let data = data else {
+                print("No data returned from server.")
+                return
             }
-            // handle response
-//            if let httpResponse = response as? HTTPURLResponse,
-//               httpResponse.statusCode == 200 {
-//                isLoggedIn = true
+            if let content = String(data: data, encoding: .utf8) {
+                print(content)
+            }
+//            if let data,
+//              let content = String(data: data, encoding: .utf8) {
+//               print(content)
+//            }
             if let httpResponse = response as? HTTPURLResponse,httpResponse.statusCode != 200 {
                 print("httpResponse.statusCode: \(httpResponse.statusCode)")
                 errorMessage = "帳號或密碼輸入錯誤"
             } else {
                 let decoder = JSONDecoder()
+                do {
+                    let userData = try decoder.decode(UserData.self, from: data)
+                    print("============== userData ==============")
+                    print(userData)
+                    print("使用者名稱為：\(userData.username)")
+                    getUsername = userData.username
+                    print("============== userData ==============")
+                } catch {
+                    print("解碼失敗：\(error)")
+                }
+
+//                if let userData = try? decoder.decode(UserData.self, from: data) {
+//                    print("============== userData ==============")
+//                    print(userData)
+//                    print("使用者名稱為：\(userData.username)")
+//                    print("============== userData ==============")
+//               }
                 isLoggedIn = true
                 print(isLoggedIn)
-//            guard let data = data else { return }
-//            print(String(data: data, encoding: .utf8)!)
-//            do {
-//                let jsonResponse = try JSONSerialization.jsonObject(with: data, options: [])
-//                guard let dict = jsonResponse as? [String: Any], let status = dict["status"] as? String else { return }
-//                if status == "success" {
-//                    self.isLoggedIn = true
-//                } else {
-//                    print("Login failed")
-//                }
-//            } catch {
-////                print(error.localizedDescription)
-//                print(String(describing: error))
             }
         }.resume()
     }
